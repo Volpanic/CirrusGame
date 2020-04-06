582f6278203c3d71729bea058544c479da9773e2
Volpanic <volpanicstudios@gmail.com>
2020-04-06 12:25:05 +1000

    Updated readme, and added draw text function into the drawing class.

--- Cirrus/Cirrus/Cirrus/Helpers/Drawing.cs
+++ Cirrus/Cirrus/Cirrus/Helpers/Drawing.cs
@@ -25,5 +25,36 @@ namespace Cirrus.Cirrus.Helpers
 
             spriteBatch.Draw(tex,new Vector2(rect.X,rect.Y),col);
         }
+
+        public static void DrawTextExt(SpriteBatch spriteBatch, SpriteFont font, string text, Vector2 position, TextAlign hori, TextAlign vert, Vector2 Scale, Color col)
+        {
+            Vector2 size = font.MeasureString(text);
+            Vector2 offset = new Vector2();
+
+            switch(hori)
+            {
+                case TextAlign.Left: break;
+                case TextAlign.Center: offset.X = size.X/2; break;
+                case TextAlign.Right: offset.X = size.X; break;
+            }
+
+            switch (vert)
+            {
+                case TextAlign.Left: break;
+                case TextAlign.Center: offset.Y = size.Y / 2; break;
+                case TextAlign.Right: offset.Y = size.Y; break;
+            }
+
+            offset *= Scale;
+
+            spriteBatch.DrawString(font, text, position - offset, col, 0.0f, Vector2.Zero, Scale, SpriteEffects.None, 0.0f);
+        }
+    }
+
+    public enum TextAlign
+    {
+        Left = 0,
+        Center = 1,
+        Right = 2
     }
 }
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A simple platformer based in Monogame, currently not much of a game just a basic
 Accurate map scrolling (Insead of just displaying to whole level on screen)
 Entity placing and editing tool,
 Saving and loading maps,
+basic event script (or just list of commands to pick from)(Similar to Cave Story's [TSC](https://www.cavestory.org/guides/basicmodding/guide/tsc.html) ) 
 
-**Stretch goals**
-basic event script (Similar to Cave Story's [TSC](https://www.cavestory.org/guides/basicmodding/guide/tsc.html) ) 
+Running the game at this point won't do much, mainly because right now the editor is the main focus, running the game in editor will load the made level into the game.
